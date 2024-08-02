66a5bf8fffd9c5a8576d5148ec3d05c4051a6ff7
EderBraganca <88894912+EderBraganca@users.noreply.github.com>
2024-08-02 08:28:15 -0300

    Create and start Spring

---
+++ src/main/java/com/ml/meLiga/MeLigaApplication.java
@@ -0,0 +1,13 @@
+package com.ml.meLiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeLigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeLigaApplication.class, args);
+	}
+
+}
