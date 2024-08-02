f381985389fb4b8d647630fdbb4c416a3c26fe0d
EderBraganca <88894912+EderBraganca@users.noreply.github.com>
2024-08-02 09:36:35 -0300

    Update User domain

--- src/main/java/com/ml/meLiga/domain/User.java
+++ src/main/java/com/ml/meLiga/domain/User.java
@@ -1,16 +1,27 @@
 package com.ml.meLiga.domain;
 
+import com.ml.meLiga.dtos.UserDTO;
+import jakarta.persistence.*;
 import lombok.*;
 
+@Entity(name="users")
+@Table(name="users")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(of="id")
 public class User {
-
+    @Id
+    @GeneratedValue
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
 
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.email = data.email();
+    }
 }
